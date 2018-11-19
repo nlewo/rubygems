a7e45c056a57efb44fe40be9498ab4dc09ebf535
Antoine Eiche <lewo@abesis.fr>
2018-11-19 18:36:08 +0100

    Run distclean after installation
    This removes files generated at build time that are no longer
    required. The main purposes is to allow reproducible build but this
    also saves disk space.
    
    Indeed, the `Makefile` and `mkmf.log` are not reproducbile since they
    contain temporary filepaths.

--- lib/rubygems/ext/builder.rb
+++ lib/rubygems/ext/builder.rb
@@ -40,7 +40,7 @@ class Gem::Ext::Builder
 
     destdir = '"DESTDIR=%s"' % ENV['DESTDIR']
 
-    ['clean', '', 'install'].each do |target|
+    ['clean', '', 'install', 'distclean'].each do |target|
       # Pass DESTDIR via command line to override what's in MAKEFLAGS
       cmd = [
         make_program,
